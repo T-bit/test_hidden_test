97481af90c012c2c2ae6697f0e97ad71464e7082
T-bit <tibit.michelangelo@gmail.com>
2025-12-10 19:13:46 +0300

    Added ListScope and EnumerableExtensions

---
+++ Assets/Scripts/Runtime/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace HiddenTest.Extensions
+{
+    [PublicAPI]
+    public static class EnumerableExtensions
+    {
+        public static List<T> ToList<T>(this IEnumerable<T> self, List<T> list, bool append = false)
+        {
+            if (!append)
+            {
+                list.Clear();
+            }
+
+            list.AddRange(self);
+
+            return list;
+        }
+
+        public static bool TryGetFirst<T>(this IEnumerable<T> self, Predicate<T> predicate, out T result)
+        {
+            foreach (var item in self)
+            {
+                if (predicate != null && !predicate.Invoke(item))
+                {
+                    continue;
+                }
+
+                result = item;
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+
+        public static bool TryGetFirst<T>(this IEnumerable<T> self, out T result)
+        {
+            return self.TryGetFirst(null, out result);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Utilities/PoolUtility.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine.Assertions;
+
+namespace HiddenTest.Utilities
+{
+    [PublicAPI]
+    public static class PoolUtility<T>
+        where T : class, new()
+    {
+        private static readonly Stack<T> Stack = new();
+
+        public static void Push(T value)
+        {
+            Assert.IsFalse(Stack.Contains(value));
+            Stack.Push(value);
+        }
+
+        public static T Pull()
+        {
+            return Stack.Count > 0
+                ? Stack.Pop()
+                : new T();
+        }
+    }
+
+    [PublicAPI]
+    public static class PoolUtility
+    {
+        public static void PushList<T>(List<T> value)
+        {
+            PoolUtility<List<T>>.Push(value);
+        }
+
+        public static List<T> PullList<T>()
+        {
+            return PoolUtility<List<T>>.Pull();
+        }
+    }
+}
