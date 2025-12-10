b915a0275264e848b397af07703d3aa6312ee4bd
T-bit <tibit.michelangelo@gmail.com>
2025-12-10 03:34:31 +0300

    Base Game scene structure

---
+++ Assets/Scripts/Runtime/StateControllers/StateController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+
+namespace HiddenTest.StateControllers
+{
+    [Serializable]
+    public abstract class StateController : IAsyncStartable, IDisposable
+    {
+        protected virtual UniTask OnStartAsync(CancellationToken cancellationToken)
+        {
+            return UniTask.CompletedTask;
+        }
+
+        protected virtual void OnDispose()
+        {
+        }
+
+        #region IAsyncStartable
+
+        UniTask IAsyncStartable.StartAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return OnStartAsync(cancellationToken);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        void IDisposable.Dispose()
+        {
+            OnDispose();
+        }
+
+        #endregion
+    }
+}
