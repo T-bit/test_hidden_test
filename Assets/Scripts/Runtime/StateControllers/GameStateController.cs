e32debb615cafca343b04bf3ecc749544e1d8fa4
T-bit <tibit.michelangelo@gmail.com>
2025-12-10 19:15:07 +0300

    Added check ids logic

--- Assets/Scripts/Runtime/StateControllers/GameStateController.cs
+++ Assets/Scripts/Runtime/StateControllers/GameStateController.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using HiddenTest.Extensions;
 using HiddenTest.Level;
+using HiddenTest.Scopes;
 using JetBrains.Annotations;
 using UnityEngine;
 using VContainer;
@@ -13,23 +16,46 @@ namespace HiddenTest.StateControllers
     {
         private readonly GeneralSettings _generalSettings;
         private readonly LevelSettings _levelSettings;
-
+        private readonly List<ObjectSettings> _objectSettingsList;
         private LevelView _levelView;
+
+        private float TimerSeconds => _generalSettings.TimerSeconds;
+        private string WinMessage => _generalSettings.WinMessage;
+        private string LooseMessage => _generalSettings.LooseMessage;
 
         public GameStateController(GeneralSettings generalSettings, LevelSettings levelSettings, Transform rootTransform, IObjectResolver container)
             : base(rootTransform, container)
         {
             _generalSettings = generalSettings;
             _levelSettings = levelSettings;
+            _objectSettingsList = new List<ObjectSettings>();
         }
 
         protected override UniTask OnStartAsync(CancellationToken cancellationToken)
         {
             _levelView = Container.Instantiate(_levelSettings.LevelViewPrefab, RootTransform);
 
-            foreach (var objectSettings in _levelSettings.ObjectSettingsList)
+            using (ListScope<ObjectView>.Create(out var objectViews))
             {
-                Debug.Log(objectSettings.Id);
+                _levelView.ObjectViews.ToList(objectViews);
+
+                foreach (var objectSettings in _levelSettings.ObjectSettingsList)
+                {
+                    if (!objectViews.TryGetFirst(item => item.Id == objectSettings.Id, out var objectView))
+                    {
+                        Debug.LogWarning($"Couldn't find object view with id {objectSettings.Id}");
+                        continue;
+                    }
+
+                    _objectSettingsList.Add(objectSettings);
+
+                    // TODO: UI Initialization
+                }
+
+                foreach (var objectView in objectViews)
+                {
+                    Debug.LogWarning($"Couldn't find object settings with id {objectView.Id}");
+                }
             }
 
             return UniTask.CompletedTask;
