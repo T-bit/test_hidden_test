70e70d602f972d708cdaddcf0fe57b40c0dbce8f
T-bit <tibit.michelangelo@gmail.com>
2025-12-16 19:57:06 +0300

    Preserved fragment models

--- Assets/Scripts/Runtime/UI/Fragments/LevelScreen/LevelScreenModel.cs
+++ Assets/Scripts/Runtime/UI/Fragments/LevelScreen/LevelScreenModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
 using HiddenTest.Level;
 using JetBrains.Annotations;
+using UnityEngine.Scripting;
 
 namespace HiddenTest.UI
 {
     [UsedImplicitly]
+    [Preserve]
     public sealed class LevelScreenModel : FragmentModel
     {
         public float Timer { get; set; }
